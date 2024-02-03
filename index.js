7dcae474877e9f27bdde57ec40884a639ab8969e
Cai Jianting <sarahmaliwat.funtrip@gmail.com>
2024-02-03 01:31:49 +0000

    prisma

---
+++ index.js
@@ -0,0 +1,38 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.user.create({
+    data: {
+     password: "123admin",
+      name: 'Herbert Asis',
+      email: 'mickmickasis123@gmail.com',
+      posts: {
+        create: { title: 'Hello World' },
+      },
+      profile: {
+        create: { bio: 'I like turtles' },
+      },
+    },
+  })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+  console.dir(allUsers, { depth: null })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
